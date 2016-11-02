26295f141cdfe6cbb52db9aaf3218c1677e31526
leo90ae <leo90ae@gmail.com>
2016-11-02 01:34:27 -0300

    Fix  findById from String to ObjectId

--- src/main/java/hello/MarkerRepository.java
+++ src/main/java/hello/MarkerRepository.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,7 +11,7 @@ import java.util.List;
  * Created by leo on 02/11/16.
  */
 @RepositoryRestResource(collectionResourceRel = "marker", path = "marker")
-public interface MarkerRepository extends MongoRepository<Marker, Integer> {
+public interface MarkerRepository extends MongoRepository<Marker, ObjectId> {
 
     List<Marker> findById(@Param("id") Integer id);
 }
